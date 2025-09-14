67d9f5e5f2cbe771d583660c44f3c34ac97152a0
Your Name <you@example.com>
2025-09-14 14:12:04 +0300

    added numbers 1 to 6 unorginazied

--- main.py
+++ main.py
@@ -7,6 +7,16 @@ constructible = {
     3486: (lambda: sum(range(ord(min(str(not()))))), "sum(range(ord(min(str(not())))))"),
 }
 
+extra_constructible = {
+    'int(not(not()))': (lambda: int(not(not())), 'int(not(not()))'),
+    'int(not())': (lambda: int(not()), 'int(not())'),
+    'len(str(ord(min(str(not())))))': (lambda: len(str(ord(min(str(not()))))), 'len(str(ord(min(str(not())))))'),
+    'len(bin(int(not())))': (lambda: len(bin(int(not()))), 'len(bin(int(not())))'),
+    'len(str(not()))': (lambda: len(str(not())), 'len(str(not())))'),
+    'len(bin(len(str(not()))))': (lambda: len(bin(len(str(not())))), 'len(bin(len(str(not()))))'),
+    'sum(range(len(str(not()))))': (lambda: sum(range(len(str(not())))), 'sum(range(len(str(not()))))'),
+}
+
 def show_constructible(d=constructible):
     print("Constructible numbers so far:\n")
     for value, (func, expr_str) in d.items():
@@ -16,5 +26,15 @@ def show_constructible(d=constructible):
         except Exception as e:
             print(f"{value:6} -> ERROR ({e}) | {expr_str}")
 
+def show_extra_constructible(d=extra_constructible):
+    print("Extra constructible numbers:\n")
+    for name, (func, expr_str) in d.items():
+        try:
+            expr_val = func()
+            print(f"{expr_val} = {expr_str}")
+        except Exception as e:
+            print(f"{name:25s} -> ERROR ({e}) | {expr_str}")
+
 if __name__ == "__main__":
     show_constructible()
+    show_extra_constructible()
